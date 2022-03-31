c8c0e8feb31e0126c992bc6b4bd023d508317ad7
buscarato <83957118+buscarato@users.noreply.github.com>
2022-03-30 23:45:07 -0300

    JS calculos
    quase todos os calculos prontos, só falta a questão de calculo total e calculo parcial

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
+
+// só para já preenhcer os daods facilitar o trampo 
+document.getElementById('tbPesoIncialKG').value = 45000
+document.getElementById('tbPesoFinalKG').value = 44450
+document.getElementById('tbValorTonelada').value = 22
+document.getElementById('tbValorMercadoria').value = 15000
+document.getElementById('tbTolerancia').value = 1
+
+
 let botao = document.getElementById('btnAdd')
 
 
 function calcular() {
-
 
     let PesoIncialKG = Number(document.getElementById('tbPesoIncialKG').value)
     let PesoFinalKG = Number(document.getElementById('tbPesoFinalKG').value)
@@ -16,37 +24,49 @@ function calcular() {
     let IR = Number(document.getElementById('tbIR').value)
     let SEST_SENAT = Number(document.getElementById('tbSEST_SENAT').value)
     let Outros = Number(document.getElementById('tbOutros').value)
-    let teste = document.getElementById('horaConsulta')
 
 
-    //calcular total de KG de saida e de chegada para saber quando KG perdeu na estrada
-    let TotalKG = PesoIncialKG - PesoFinalKG
-    //Pega quantos KG ele perdeu e multipica pelo valor da Carga
-    let TotalValorKG = TotalKG * ValorMercadoria
-    //Descobre o valor do KG da mercadoria e calculca quanto em R$ se perdeu na estrada
-    let TotalQuebra = (ValorMercadoria / PesoIncialKG) * TotalKG
-    //descobre o tatol de kg que pode ser pedido no transporte (tolerancia q a transportadora aceita)
-    let TotalExcedenteAceito = (Tolerancia / 100) * PesoIncialKG
-    //calcula quanto passou da tolerancia KG inicial menos o que podia perder 
-    let TotalExcedente = TotalKG - TotalExcedenteAceito
-    let TotalREaisPesoFinal = ValorMercadoria / PesoIncialKG
-    let Descobri = ValorTonelada * PesoFinalKG / 1000
-    let TotalQuebraAdescontar = TotalExcedente * TotalREaisPesoFinal
+    //1° Calcula peso perdido na estrada 
+    //2° Quanto a transportadora aceita perder % de perca do kg inicial
+    // Ex. carga 45.000 KG a trasnportadora aceitar perder 1% = 450 KG na estrada 
+    let SaldoTolerancia = (PesoIncialKG - PesoFinalKG) - (PesoIncialKG * Tolerancia / 100)
 
-    //let TotalFInal = TotalValorKG - Adiantamento + Pedagio - INSS - IR - SEST_SENAT - Outros - TotalQuebraAdescontar
-    let TotalFInal = Descobri - TotalQuebraAdescontar
+    let TotalQuebra = (ValorMercadoria / PesoIncialKG) * SaldoTolerancia
 
 
+    //Validação para caso não tenha quebra o sistema não adicione valores negativos
+    if (TotalQuebra < 0) {
+        TotalQuebra = 0
+    }
 
-    console.log(TotalQuebraAdescontar.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })) // ok 
-    console.log(Descobri.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
-    console.log(TotalFInal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }))
+
+    if (document.getElementById("radioExcedente").checked) {
+        //1° Calcula valor Mercadoria em KG
+        //2° Calcula campo1 vezes o SaldoTolerancia para descobrir a perca
+        //3° Ex R$ 15.000,00 X 45.000 KG = R$ 0,333  Neste exemplo ele podia ter pedido 450kg
+        // mais ele perdeu 550 kg logo perdeu 100kg a mais  100(KG) * R$ 0,33 = R$ 33,33 total quebra    
+        // let TotalQuebra = (ValorMercadoria / PesoIncialKG) * SaldoTolerancia
+        console.log('radioExcedente')
+
+        // //Validação para caso não tenha quebra o sistema não adicione valores negativos
+        // if (TotalQuebra < 0) {
+        //     TotalQuebra = 0
+    }
+    else if (document.getElementById("radioTotal").checked) {
+        //1° A transportadora combra o valor total não tem tolerancia
+        //let TotalQuebra = (ValorMercadoria / PesoIncialKG) * (PesoIncialKG - PesoFinalKG)
+        console.log('radioTotal')
+    }
 
 
-    document.getElementById('tbQuebra').value = TotalQuebraAdescontar.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })// ok 
-    document.getElementById('tbTotalCartaFrete').value = Descobri.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-    document.getElementById('tbTotalReceber').value = TotalFInal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
+    //Calculos valores finais 
+    let TotalBrutoFrete = (ValorTonelada * PesoFinalKG) / 1000
+    let TotalLiquido = TotalBrutoFrete - TotalQuebra - Adiantamento + Pedagio + Premio - INSS - IR - SEST_SENAT - Outros
+
+    document.getElementById('tbQuebra').value = TotalQuebra.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })// ok 
+    document.getElementById('tbTotalCartaFrete').value = TotalBrutoFrete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    document.getElementById('tbTotalReceber').value = TotalLiquido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
 
 }
@@ -54,10 +74,3 @@ function calcular() {
 botao.addEventListener('click', calcular)
 
 
-
-//TotalKG = TotalKG
-//ValorTotalKG = TotalValorKG
-//QuebraTotal = TotalQuebra
-//SaldoToleranciaExcedente = TotalExcedenteAceito
-//ToleranciaExcedente = TotalExcedente
-//TotalExcedente = TotalQuebraAdescontar
